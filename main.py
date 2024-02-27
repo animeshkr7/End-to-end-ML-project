428b1df18df58b0b0687792998e7fd3d7f93df56
animeshkr7 <animeshkr7@gmail.com>
2024-02-27 19:22:57 +0530

    logging

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from src.mlproject.config import configuration
+# from src.mlproject.config import configuration   # for checking setup.py 
+
+#for logging
+from src.mlproject.logging import logger
+
+logger.info("TRY CASE ")
+# StreamHandler cone shown in terminal,fileHandler other saved in logs folder 
